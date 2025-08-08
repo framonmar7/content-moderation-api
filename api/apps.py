d87d80b67d1525928276d0d2aa80dcd39a3d8add
Francisco Jesús <framonmar7@gmail.com>
2025-08-08 00:15:38 +0000

    feat: load AI models on application startup

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from api.inference import models
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        models.load_all_models()
--- api/inference/models.py
+++ api/inference/models.py
@@ -1,10 +1,16 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 
+toxic_classifier = None
+insult_classifier = None
+hate_classifier = None
+
 def load_pipeline(model_repo: str) -> TextClassificationPipeline:
     tokenizer = AutoTokenizer.from_pretrained(model_repo)
     model = AutoModelForSequenceClassification.from_pretrained(model_repo)
     return TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
-toxic_classifier = load_pipeline("framonmar7/toxic-classifier")
-insult_classifier = load_pipeline("framonmar7/insult-classifier")
-hate_classifier = load_pipeline("framonmar7/hate-classifier")
+def load_all_models():
+    global toxic_classifier, insult_classifier, hate_classifier
+    toxic_classifier = load_pipeline("framonmar7/toxic-classifier")
+    insult_classifier = load_pipeline("framonmar7/insult-classifier")
+    hate_classifier = load_pipeline("framonmar7/hate-classifier")
